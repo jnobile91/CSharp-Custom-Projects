b5f629310928cf68e70f2ba149c8f9daec5725af
jnobile91 <jnobile91@live.com>
2021-05-20 13:28:17 -0400

    Add "Create" functionality with server-side and client-side validation

--- BookListRazor/BookListRazor/Pages/BookList/Create.cshtml.cs
+++ BookListRazor/BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -17,11 +17,27 @@ namespace BookListRazor.Pages.BookList
             _db = db;
         }
 
+        [BindProperty]
         public Book Book { get; set; }
 
         public void OnGet()
         {
 
+        }
+
+        // No argument is needed here, as we set "Book Book" as a [BindProperty] in line 20
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book); // Adds info from the object to be added from the "Create" form into a queue
+                await _db.SaveChangesAsync(); // Adds info to DB
+                return RedirectToPage("Index"); // Returns user to the Index page after entering a new object into the DB
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
